f9e4f3d53b8c4c379d0999ffc421bb311dab3333
sachadvr <sachadvr@icloud.com>
2023-03-28 16:17:00 +0200

    fixed script for show.html.twig

--- migrations/Version20230328141136.php
+++ migrations/Version20230328141136.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230316091356 extends AbstractMigration
+final class Version20230328141136 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,8 +21,14 @@ final class Version20230316091356 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE commentaires_hashtag (commentaires_id INT NOT NULL, hashtag_id INT NOT NULL, INDEX IDX_B829727717C4B2B0 (commentaires_id), INDEX IDX_B8297277FB34EF56 (hashtag_id), PRIMARY KEY(commentaires_id, hashtag_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE hashtag (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE post_hashtag (post_id INT NOT NULL, hashtag_id INT NOT NULL, INDEX IDX_675D9D524B89032C (post_id), INDEX IDX_675D9D52FB34EF56 (hashtag_id), PRIMARY KEY(post_id, hashtag_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE commentaires_hashtag ADD CONSTRAINT FK_B829727717C4B2B0 FOREIGN KEY (commentaires_id) REFERENCES commentaires (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE commentaires_hashtag ADD CONSTRAINT FK_B8297277FB34EF56 FOREIGN KEY (hashtag_id) REFERENCES hashtag (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE post_hashtag ADD CONSTRAINT FK_675D9D524B89032C FOREIGN KEY (post_id) REFERENCES post (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE post_hashtag ADD CONSTRAINT FK_675D9D52FB34EF56 FOREIGN KEY (hashtag_id) REFERENCES hashtag (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE post ADD allowcommentaire TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD verified INT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
@@ -30,6 +36,12 @@ final class Version20230316091356 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE commentaires_hashtag DROP FOREIGN KEY FK_B829727717C4B2B0');
         $this->addSql('ALTER TABLE commentaires_hashtag DROP FOREIGN KEY FK_B8297277FB34EF56');
+        $this->addSql('ALTER TABLE post_hashtag DROP FOREIGN KEY FK_675D9D524B89032C');
+        $this->addSql('ALTER TABLE post_hashtag DROP FOREIGN KEY FK_675D9D52FB34EF56');
         $this->addSql('DROP TABLE commentaires_hashtag');
+        $this->addSql('DROP TABLE hashtag');
+        $this->addSql('DROP TABLE post_hashtag');
+        $this->addSql('ALTER TABLE user DROP verified');
+        $this->addSql('ALTER TABLE post DROP allowcommentaire');
     }
 }
